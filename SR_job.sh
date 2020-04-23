b93cf80698cff548dcd6d4cbbd25ddacf0dda3b9
Hong Pan <61482421+Hong-Swinburne@users.noreply.github.com>
2020-04-23 16:37:11 +1000

    Add files via upload

--- SR_job.sh
+++ SR_job.sh
@@ -9,11 +9,11 @@
 
 module load cudnn/7.0.5-cuda-9.0.176
 
-cp -a ~/../../fred/oz138/COS80024/EO_Degradation/EO_Data/Beach/beach1/. ~/../../fred/oz138/COS80024/torch/NTIRE2017/demo/img_input/
+cp -a ~/../../fred/oz138/COS80024/EO_Degradation/EO_Data/Rural/rural1/. ~/../../fred/oz138/COS80024/torch/NTIRE2017/demo/img_input/
 
 th test.lua -model EDSR_x2 -selfEnsemble true
 
-mv -v ~/../../fred/oz138/COS80024/torch/NTIRE2017/demo/img_output/EDSR_x2/myImages/X2/* ~/../../fred/oz138/COS80024/EO_Degradation/EO_Data/Beach/beach1_SR/
+mv -v ~/../../fred/oz138/COS80024/torch/NTIRE2017/demo/img_output/EDSR_x2/myImages/X2/* ~/../../fred/oz138/COS80024/EO_Degradation/EO_Data/Rural/rural1_SR/
 
 cd ~/../../fred/oz138/COS80024/torch/NTIRE2017/demo/img_input
 
